273497aed48b037503a80d0c9c6fcd1d4a21213f
HiroKN <88582003+nanten-pi@users.noreply.github.com>
2024-05-29 22:43:36 +0900

    画像を動かせた

--- sample.py
+++ sample.py
@@ -6,10 +6,19 @@ def main():
     screen = pygame.display.set_mode((600, 500))                # 大きさ600*500の画面を生成
     pygame.display.set_caption("image")                         # タイトルバーに表示する文字
     logo = pygame.image.load("test.png")                     #　ペンギンの画像取得
-
+    px=20
+    py=50
     while (1):
         screen.fill((0,0,0))                                    # 画面全体を黒色に塗りつぶし
-        screen.blit(logo,(20,50))                               # 左上(20,50)の配置に画像描画（表示）
+        if(px<600):
+            px+=0.1
+        else:
+            px=20
+        if(py<500):
+            py+=0.1
+        else:
+            py=50
+        screen.blit(logo,(px,py))                               # 左上(20,50)の配置に画像描画（表示）
         pygame.display.update()                                 # 画面を更新
         # イベント処理
         for event in pygame.event.get():
